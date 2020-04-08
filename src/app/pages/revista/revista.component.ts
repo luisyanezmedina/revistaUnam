afa009df7495cea6f6eb81df810e425186941437
Jose Luis Yañez Medina <luisyanez@MacBook-Pro-de-Jose.local>
2020-04-08 15:37:32 -0500

    Router para revistas

--- src/app/pages/revista/revista.component.ts
+++ src/app/pages/revista/revista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-revista',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RevistaComponent implements OnInit {
 
-  constructor() { }
+  revistaId: any;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.revistaId = this.route.snapshot.paramMap.get('id');
   }
 
 }
